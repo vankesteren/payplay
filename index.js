5f38069fede0a0b8f105cf7215876be9f592c52d
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2024-11-14 11:26:02 +0100

    small update again

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const contentElement = document.getElementById("content");
+function log(message) {content.innerHTML += `<p>${message}</p>`};
 
-function log(message) {
-    contentElement.innerHTML += "<p>";
-    contentElement.innerHTML += message;
-    contentElement.innerHTML += "</p>";
+async function onclick() {
+  startButton.style.display = "none";
+  try {
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    log("Scan started");
+    ndef.addEventListener("readingerror", () => {
+      log("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      log(`> Serial Number: ${serialNumber}`);
+      log(`> Records: (${message.records.length})`);
+    });
+  }
+  catch (error) {
+    log("Argh! " + error);
+  }
 }
 
 if ("NDEFReader" in window) {
-    log("Browser supported");
-    try {
-      const ndef = new NDEFReader();
-      await ndef.scan();
-      log("Scan started");
-      ndef.addEventListener("readingerror", () => {
-        log("Argh! Cannot read data from the NFC tag. Try another one?");
-      });
-      ndef.addEventListener("reading", ({ message, serialNumber }) => {
-        log(`> Serial Number: ${serialNumber}`);
-        log(`> Records: (${message.records.length})`);
-      });
-    }
-    catch (error) {
-      log("Argh! " + error);
-    }
+  log("Browser supported");
+  startButton.addEventListener("click", onclick);
 } else {
-    log("Browser not supported");
+  log("Browser not supported");
+  startButton.style.display = "none";
 }
+
+
