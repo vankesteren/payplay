ab5e6ecb9a4207cb3cfde3240f16d1bfceb0db91
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2024-11-14 12:03:17 +0100

    add color thingy

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-function log(message) {content.innerHTML += `<p>${message}</p>`};
+function log(message) {logging.innerHTML += `<p>${message}</p>`};
+function changecolor() {
+  const bgcols = ["red", "green", "blue", "pink", "orange", "purple", "magenta"];
+  const fgcols = ["white", "white", "white", "black", "black", "white", "white"];
+  var idx = Math.floor(Math.random() * fgcols.length)
+  document.documentElement.style.setProperty('--base-color', bgcols[idx]);
+  document.documentElement.style.setProperty('--contrast-color', fgcols[idx]);
+}
 
-if ("NDEFReader" in window) {
-  log("Browser supported");
-  startButton.addEventListener("click", async () => {
-    startButton.style.display = "none";
+async function main() {
+  startButton.style.display = "none";
     try {
       const ndef = new NDEFReader();
       await ndef.scan();
       log("Scan started");
       ndef.addEventListener("readingerror", () => {
         log("Argh! Cannot read data from the NFC tag. Try another one?");
+        changecolor();
       });
       ndef.addEventListener("reading", ({ message, serialNumber }) => {
         log(`> Serial Number: ${serialNumber}`);
@@ -19,10 +25,15 @@ if ("NDEFReader" in window) {
     catch (error) {
       log("Argh! " + error);
     }
-  });
+}
+
+// Actually register the functions
+if ("NDEFReader" in window) {
+  log("Browser supported");
+  startButton.addEventListener("click", async () => { await main() });
 } else {
   log("Browser not supported");
-  startButton.style.display = "none";
+  startButton.addEventListener("click", changecolor);
 }
 
 
